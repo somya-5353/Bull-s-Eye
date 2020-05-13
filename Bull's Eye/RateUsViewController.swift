f150bb5ff53c0f5bd825f910d80ba1c482293a47
Somya-5353 <somyacarmel5353@gmail.com>
2020-05-13 21:59:57 +0530

    buttons added to rate us screen

--- Bull's Eye/RateUsViewController.swift
+++ Bull's Eye/RateUsViewController.swift
@@ -15,16 +15,10 @@ class RateUsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func close(_ sender: Any) {
+        
+    dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
