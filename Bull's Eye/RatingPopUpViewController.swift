e7e2fc01cff2b13c405a071b0e2460e46751d7a6
Somya-5353 <somyacarmel5353@gmail.com>
2020-05-14 23:30:06 +0530

    custom type alert view

---
+++ Bull's Eye/RatingPopUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RatingPopUpViewController.swift
+//  Bull's Eye
+//
+//  Created by Somya on 14/05/20.
+//  Copyright © 2020 Somya. All rights reserved.
+//
+
+import UIKit
+
+class RatingPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
